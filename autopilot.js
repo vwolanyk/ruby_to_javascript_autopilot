16c59ee541bd93ebe2b032e45f64970b9e725870
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-29 15:44:59 -0400

    Add new_car function, and getNewCar fuction - tested, works

---
+++ autopilot.js
@@ -0,0 +1,21 @@
+function getNewCar() {
+
+   return {city: 'Toronto',
+  passengers: 0,
+  gas: 100}
+
+}
+
+
+var cars = []
+console.log(cars)
+function addCar(cars,newCar) {
+cars.push(newCar)
+return "Adding new car to fleet. Fleet size is now" = "."
+}
+
+var newCar= getNewCar()
+
+addCar(cars,newCar)
+
+console.log(cars)
